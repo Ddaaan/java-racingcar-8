4807c5578470a7e9027e9839449f01943e9ded25
Ddaaan <de2572@naver.com>
2025-10-27 21:35:10 +0900

    feat(game): 라운드 진행하며 실행결과, 최종 우승자 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,20 @@
 package racingcar;
 
 import java.util.List;
+import racingcar.game.Racing;
 import racingcar.view.InputView;
+import racingcar.view.ResultView;
 
 public class Application {
   public static void main(String[] args) {
     // TODO: 프로그램 구현
     try {
       InputView inputView = new InputView();
+
       List<String> carNames = inputView.readCarNames();
       int attempts = inputView.readAttemptCount();
+
+      new Racing(carNames, new ResultView()).run(attempts);
     } catch (IllegalArgumentException e) {
       System.out.println(e.getMessage());
     }
--- src/main/java/racingcar/game/Racing.java
+++ src/main/java/racingcar/game/Racing.java
@@ -4,18 +4,24 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
+import racingcar.view.ResultView;
 
 public class Racing {
   private final List<Car> cars;
+  private final ResultView view;
 
-  public Racing(List<String> carNames) {
+  public Racing(List<String> carNames, ResultView view) {
     this.cars = toCars(carNames);
+    this.view = view;
   }
 
   public void run(int attempts) {
+    view.printStart();
     for (int i = 0; i < attempts; i++) {
       playOneRound();
+      view.printRound(cars);
     }
+    view.printWinners(findWinners());
   }
 
   private void playOneRound() {
@@ -31,5 +37,28 @@ public class Racing {
       list.add(new Car(n));
     }
     return list;
+  }
+
+  private List<Car> findWinners() {
+    int max = getMaxPosition();
+    List<Car> winners = new ArrayList<>();
+    for (Car car : cars) {
+      if (car.getPosition() == max) {
+        winners.add(car);
+      }
+    }
+
+    return winners;
+  }
+
+  private int getMaxPosition() {
+    int max = 0;
+    for (Car car : cars) {
+      if (car.getPosition() > max) {
+        max = car.getPosition();
+      }
+    }
+
+    return max;
   }
 }
