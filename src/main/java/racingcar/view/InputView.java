68f5beb6653e8a78f8e49ce4a70a333c6ce4740a
Ddaaan <de2572@naver.com>
2025-10-27 20:16:40 +0900

    feat(input): 자동차 이름 입력받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,15 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class InputView {
+  private static final String INPUT_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+  public List<String> readCarNames() {
+    System.out.println(INPUT_PROMPT);
+    String link = Console.readLine();
+
+  }
+
 }
